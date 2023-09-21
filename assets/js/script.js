2c6cd02ae82e93a1a9fde450c40c0f0d45f8bef1
elemeismo <145720578+elemeismo@users.noreply.github.com>
2023-09-21 18:56:11 -0300

    luisa

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var nome = window.prompt('qual é o seu nome?') 
+switch(nome){
+    case 'luisa':
+        window.alert('esse é o nome mais lindo do mundo!!! eu te amo!!!')
+        break
+    case 'Luisa':
+        window.alert('esse é o nome mais lindo do mundo!!! você é maravilhosa!!!')
+        break
+    case 'yan':
+        window.alert('yan??? esse é o cara mais lindo que existe na terra')
+        break
+    case 'Yan':
+        window.alert('Yan??? esse aí é um cara gostoso demais!')
+        break
+    default:
+        window.alert('nome fei da porra')
+}
